84c4e701982dede08a9a2c947c96e66d797bb3d3
MbarekAmehry <mbarek.amehry@gmail.com>
2021-06-11 23:37:14 +0100

    clean dev

--- app.js
+++ app.js
@@ -1,79 +1,112 @@
 $(document).ready(function () {
-  $(window).on('load', function () {
-    var $landingWrapper = $('.landing-wrapper'),
-      $landingInnerContent = $('.landing-inner-content');
-
-    $landingInnerContent.on('mousemove touchmove', function (e) {
-      var xMove = '+=0',
-        yMove = '+=0';
-      if (e.clientX > $landingWrapper.width() / 2) {
-        xMove = '+=200';
-      } else {
-        xMove = '-=200';
-      }
-      if (e.clientY > $landingWrapper.height() / 2) {
-        yMove = '+=200';
-      } else {
-        yMove = '-=200';
-      }
-
-      TweenMax.to($landingWrapper, 5, {
-        scrollTo: {
-          x: xMove,
-          y: yMove,
-        },
-        ease: Power0.easeNone,
-        overwrite: 'all',
-        repeat: -1,
-      });
-    });
+  var timeout;
+  $('#container').mousemove(function (e) {
+    if (timeout) clearTimeout(timeout);
+    setTimeout(callParallax.bind(null, e), 200);
   });
 
-  var rect = $('.landing-wrapper')[0].getBoundingClientRect();
-  // create mouse object to keep track of mouse coordinates
-  var mouse = { x: 0, y: 0, moved: false };
-
-  $('.landing-wrapper').mousemove(function (e) {
-    mouse.moved = true;
-
-    mouse.x = e.clientX - rect.left; // e = event
-    mouse.y = e.clientY - rect.top;
-  });
-
-  TweenLite.ticker.addEventListener('tick', function () {
-    if (mouse.moved) {
-      parallaxIt('.two', 1, -75);
-      parallaxIt('.one', 1, -100);
-      parallaxIt('.three', 1, 100);
-    }
-
-    mouse.moved = false;
-  });
-
-  function parallaxIt(target, duration, movement) {
-    TweenMax.to(target, duration, {
-      x: ((mouse.x - rect.width / 2) / rect.width) * movement,
-      y: ((mouse.y - rect.height / 2) / rect.height) * movement,
-      ease: Sine.easeOut,
-    }); //SlowMo.ease});
+  function callParallax(e) {
+    parallaxIt(e, '.slide', -190);
+    // parallaxIt(e, 'img', -30);
   }
 
-  // Recaclulate container dimensions on resize and scroll
-  $(window).on('resize scroll', function () {
-    rect = $('.landing-wrapper')[0].getBoundingClientRect();
+  function parallaxIt(e, target, movement) {
+    var $this = $('#container');
+    var relX = e.pageX - $this.offset().left;
+    var relY = e.pageY - $this.offset().top;
+
+    TweenMax.to(target, 1, {
+      x: ((relX - $this.width() / 2) / $this.width()) * movement,
+      y: ((relY - $this.height() / 2) / $this.height()) * movement,
+      ease: Power2.easeOut,
+    });
+  }
+
+  var mX,
+    mY,
+    distance,
+    $slides = $('.slide');
+
+  $slides.click(function () {
+    $(location).attr('href', 'slider.html');
   });
 
-  //   Zoom element on hover alongside with next ones
-  $(document).ready(function () {
-    $('.slide').on('mouseenter', function () {
-      $(this).prev().addClass('beside');
-      $(this).next().addClass('beside');
-    });
-    $('.slide').on('mouseleave', function () {
-      $(this).prev().removeClass('beside');
-      $(this).next().removeClass('beside');
-    });
-  });
+  // calc Distance between mouse and other elements
+  function calculateDistance(elem, mouseX, mouseY) {
+    return Math.floor(
+      Math.sqrt(
+        Math.pow(mouseX - (elem.offset().left + elem.width() / 2), 2) +
+          Math.pow(mouseY - (elem.offset().top + elem.height() / 2), 2)
+      )
+    );
+  }
 
-  // end zoom
+  // Zoom element on mou
+  // $slides.mouseenter(function () {
+  //   TweenMax.to($slides.not(this), 0.23, { scale: 1 });
+  //   TweenMax.to(this, 0.3, { scale: 1.852 });
+
+  //   distance = calculateDistance($slides.not(this), mX, mY);
+  //   console.log(distance);
+  //   if (distance < 400) {
+  //   } else {
+  //     TweenMax.to($slides.not(this), 0.3, { scale: 1 });
+  //   }
+  // });
+
+  // Remove zoom on mouseLeave
+  // $slides.mouseleave(function () {
+  //   TweenMax.to(this, 0.3, { scale: 1 });
+  // });
+
+  // $(document).mousemove(function (e) {
+  //   mX = e.pageX;
+  //   mY = e.pageY;
+  //   distance = calculateDistance($slides, mX, mY);
+  //   console.log(distance);
+  //   if (distance < 800) {
+  //     // $slides.addClass('beside');
+  //   }
+  // });
+
+  // $('.slide').on('mouseenter', function () {
+  //   $(this).prev().addClass('beside');
+  //   $(this).next().addClass('beside');
+  // });
+  // $('.slide').on('mouseleave', function () {
+  //   $(this).prev().removeClass('beside');
+  //   $(this).next().removeClass('beside');
+  // });
+
+  //
+
+
+  let imgs = document.getElementsByClassName('slide');
+  let handleMousemove = (e) => {
+    let range = 0;
+    for (let i = 0; i < imgs.length; i++) {
+      const img = imgs[i];
+      img.classList.remove('active');
+      if (
+        img.offsetLeft + img.offsetWidth >= e.x - range &&
+        img.offsetLeft <= e.x + range &&
+        img.offsetTop + img.offsetHeight >= e.y - range &&
+        img.offsetTop <= e.y + range
+      ) {
+        // img.classList.add('active');
+        TweenMax.to(img, 0.3, { scale: 1.6 });
+      } else {
+        TweenMax.to(img, 0.3, { scale: 1 });
+      }
+    }
+};
+
+
+for (let i = 0; i < imgs.length; i++) {
+  const img = imgs[i];
+  img.addEventListener('mouseenter', handleMousemove);
+  img.addEventListener('mouseleave', handleMousemove);
+
+}
+
 });
